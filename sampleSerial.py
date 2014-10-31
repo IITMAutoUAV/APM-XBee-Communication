8f1ee8141701083bb06c8368452b6c65f22b94d1
khalid2696 <khalid2696@gmail.com>
2014-10-31 22:35:54 +0530

    changed sample

--- sampleSerial.py
+++ sampleSerial.py
@@ -3,7 +3,7 @@ portNumber = 10
 ser = serial.Serial(portNumber)
 print ser.name
 # send "Hello" message
-ser.write("hello")
+ser.write("hello abhishek")
 # read 100 bits of data
 m = ser.read(100)
 while m=="":
