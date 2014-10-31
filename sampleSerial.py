766ed18f62269d63e48337c919be42f09b08b65f
abhiShandy <abhishek.sandy@gmail.com>
2014-10-31 21:57:29 +0530

    creating sample serial code

---
+++ sampleSerial.py
@@ -0,0 +1,13 @@
+import serial
+portNumber = 10
+ser = serial.Serial(portNumber)
+print ser.name
+# send "Hello" message
+ser.write("hello")
+# read 100 bits of data
+m = ser.read(100)
+while m=="":
+	m = ser.read(100)
+else:
+	print m
+ser.close()
