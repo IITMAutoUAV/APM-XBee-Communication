20f92dd06d2b516ff07956c482fd6b440c08657e
kallgp <kallanjegp@gmail.com>
2014-11-01 01:43:17 +0530

    added arducopter source to modify variables

--- ard_ch_all_rec/ard_ch_all_rec.ino
+++ ard_ch_all_rec/ard_ch_all_rec.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 SoftwareSerial myser(10,11);
-int ch11, ch12, ch13, ch21, ch22, ch23, ch31, ch32, ch33, ch41, ch42, ch43,a;
+int ch1,ch2,ch3,ch4,ch11, ch12, ch13, ch21, ch22, ch23, ch31, ch32, ch33, ch41, ch42, ch43,a;
 
 void setup()
 {Serial.begin(9600);
@@ -44,7 +44,11 @@ if (myser.read()=='#')
 
 if (a==1)
 {
-Serial.write(ch11);
+  ch1=ch11*100+ch12*10+ch13;
+  ch2=ch21*100+ch22*10+ch23;
+  ch3=ch31*100+ch32*10+ch33;
+  ch4=ch41*100+ch42*10+ch43;
+/*Serial.write(ch11);
 Serial.write(ch12);
 Serial.write(ch13);
 Serial.println("sera");
@@ -58,7 +62,11 @@ Serial.write(ch33);
 Serial.println("dww");
 Serial.write(ch41);
 Serial.write(ch42);
-Serial.write(ch43);
+Serial.write(ch43);*/
+Serial.write(ch1);
+Serial.write(ch2);
+Serial.write(ch3);
+Serial.write(ch4);
 }
 
 }
