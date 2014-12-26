6ffca3d1a36898a47d121a516c5d702e142af305
flamerx95 <hanut2@gmail.com>
2014-12-26 22:58:27 +0530

    apm_ser_contr.py

--- apm_contr_allch.py
+++ apm_contr_allch.py
@@ -1,19 +1,17 @@
 import serial
 import time
 import multiprocessing
-
 ser=serial.Serial("/dev/ttyUSB0", 9600)
 
 
-
-"""ch1=raw_input('chan1')
-ch2=raw_input('chan2')
-ch3=raw_input('chan3')
-ch4=raw_input('chan4')"""
-ch1='032'
-ch2='032'
-ch3='032'
-ch4='032'
+ch1=0
+ch2=0
+ch3=0
+ch4=0
+"""ch1=input('chan1')
+ch2=input('chan2')
+ch3=input('chan3')
+ch4=input('chan4')"""
 def foo(n): 
 	ser.write("#")
 
@@ -32,38 +30,15 @@ while 1:
 
     # Cleanup
     		p.join()
-	time.sleep(.004)
-	ser.write(ch1[0])	
-	time.sleep(.004)  
-	ser.write(ch1[1])
-	time.sleep(.004) 
-	ser.write(ch1[2])
-	time.sleep(.004) 
-	ser.write(ch2[0])
-	time.sleep(.004) 
-	ser.write(ch2[1])
-	time.sleep(.004) 
-	ser.write(ch2[2])
-	time.sleep(.004) 
-	ser.write(ch3[0])
-	time.sleep(.004) 
-	ser.write(ch3[1])
-	time.sleep(.004) 
-	ser.write(ch3[2])
-	time.sleep(.004) 
-	ser.write(ch4[0])
-	time.sleep(.004) 
-	ser.write(ch4[1])
-	time.sleep(.004) 
-	ser.write(ch4[2])
-
-	"""time.sleep(.004)
+	time.sleep(.002) #50ms 
+	ser.write('1')
+	time.sleep(.002)
 	ser.write('2')
-	time.sleep(.004)
+	time.sleep(.002)
 	ser.write('3')
-	time.sleep(.004)
+	time.sleep(.002)
 	ser.write('4')
-	time.sleep(.004)
+	time.sleep(.002)
 	ser.write('5')
 	time.sleep(.002)
 	ser.write('6')
@@ -80,4 +55,4 @@ while 1:
 	time.sleep(.002)
 	ser.write('4')
 	time.sleep(.002)
-	"""
+	
--- apm_ser_contr.py
+++ apm_ser_contr.py
@@ -4,56 +4,51 @@ import multiprocessing
 ser=serial.Serial("/dev/ttyUSB0", 9600)
 
 
-ch1=0
-ch2=0
-ch3=0
-ch4=0
-ch1=input('chan1')
-ch2=input('chan2')
-ch3=input('chan3')
-ch4=input('chan4')
+ch1s=0
+ch2s=0
+ch3s=0
+ch4s=0
+ch1s=raw_input('chan1')
+ch2s=raw_input('chan2')
+ch3s=raw_input('chan3')
+ch4s=raw_input('chan4')
 def foo(n): 
 	ser.write("#")
 
 while 1:
-   
-	if __name__ == '__main__':
+    if __name__ == '__main__':
     # Start foo as a process
-    		p = multiprocessing.Process(target=foo, name="Foo", args=(10,))
-    		p.start()
+        p = multiprocessing.Process(target=foo, name="Foo", args=(10,))
+        p.start()
 
     # Wait 10 seconds for foo
-    		time.sleep(.005)
+        time.sleep(.05)
 
     # Terminate foo
-    		p.terminate()
+        p.terminate()
 
     # Cleanup
-    		p.join()
-	time.sleep(.004) #50ms 
+        p.join()
+    time.sleep(.05)  
 		
 
-ch1s=str(ch1)
-ch2s=str(ch2)
-ch3s=str(ch3)
-ch4s=str(ch4)
-while len(ch1s)<3:
+    while len(ch1s)<3:
         ch1s="0"+ch1s
-while len(ch2s)<3:
+    while len(ch2s)<3:
         ch2s="0"+ch2s
-while len(ch3s)<3:
+    while len(ch3s)<3:
         ch3s="0"+ch3s
-while len(ch4s)<3:
+    while len(ch4s)<3:
         ch4s="0"+ch4s
-for i,c in ch1s:
+    for c in ch1s:
         ser.write(c)
-        time.sleep(.002)
-for i,c in ch2s:
+        time.sleep(.05)
+    for c in ch2s:
         ser.write(c)
-        time.sleep(.002)
-for i,c in ch3s:
+        time.sleep(.05)
+    for c in ch3s:
         ser.write(c)
-        time.sleep(.002)
-for i,c in ch4s:
+        time.sleep(.05)
+    for c in ch4s:
         ser.write(c)
-        time.sleep(.002)
+        time.sleep(.05)
